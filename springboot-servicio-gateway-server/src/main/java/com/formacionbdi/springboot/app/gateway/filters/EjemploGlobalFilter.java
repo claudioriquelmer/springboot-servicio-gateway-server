657b12b980da6b6229b5bbeec77090bbe6545864
claudioriquelmer <claudio.riquelme@outlook.es>
2024-05-23 15:44:01 -0400

    Configurando Gateway Filter Personalizado

--- springboot-servicio-gateway-server/src/main/java/com/formacionbdi/springboot/app/gateway/filters/EjemploGlobalFilter.java
+++ springboot-servicio-gateway-server/src/main/java/com/formacionbdi/springboot/app/gateway/filters/EjemploGlobalFilter.java
@@ -25,16 +25,16 @@ public class EjemploGlobalFilter implements GlobalFilter, Ordered {
 			
 			Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("token")).ifPresent(valor -> {
 				exchange.getResponse().getHeaders().add("token", valor);
-			})
+			});
 			
 			exchange.getResponse().getCookies().add("color", ResponseCookie.from("color", "rojo").build());
-			exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
+			//exchange.getResponse().getHeaders().setContentType(MediaType.TEXT_PLAIN);
 		}));
 	}
 
 	@Override
 	public int getOrder() {
 		// TODO Auto-generated method stub
-		return -1;
+		return 1;
 	}
 }
